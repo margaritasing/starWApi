d14f2a6719eb378a5638c5423d629d06035c7d97
margaritasing <libiamfreitesm@gmail.com>
2022-09-02 16:13:46 -0300

    subo cambios generales

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
- import CardHome from './componentes/CardPrincipal/CardHome'; 
+import CardHome from './componentes/CardPrincipal/CardHome'; 
+import Header from './componentes/Header/Header';
 
 function App() {
   return (
     <div className="App">
-    <CardHome />
+    <Header/>
+    <Routes>
+      <Route exact path='/listado' element={<CardHome />} />   
+    </Routes>
     </div>
   );
 }
