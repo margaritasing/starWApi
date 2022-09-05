c59034b8975c893fe19a9c570731204c2e0b4ef6
margaritasing <libiamfreitesm@gmail.com>
2022-09-05 14:06:08 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import CardHome from './componentes/CardPrincipal/CardHome'; 
+import Detalles from './componentes/Detalles/Detalles';
 import Header from './componentes/Header/Header';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
     <Header/>
+    <div className="container">
     <Routes>
-      <Route exact path='/listado' element={<CardHome />} />   
+    <Route exact path='/' element={<CardHome />} />   
+    <Route  path='/detalle' element={<Detalles />} />          
     </Routes>
     </div>
-  );
+    </>
+    );
 }
 
 export default App;
