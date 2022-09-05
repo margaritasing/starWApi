ad9c54edf7ca414a16d525465ac6f73024c53040
margaritasing <libiamfreitesm@gmail.com>
2022-09-05 14:07:00 -0300

    ...

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Link } from 'react-router-dom'
 
-/* import Buscador from './Buscador'; */
 
 const Header = () => {
   
@@ -19,15 +18,14 @@ const Header = () => {
             <div className="navbar-nav">            
                 <Link to="/" className="nav-link active" aria-current="page" >Home</Link>
                 <Link to="/listado" className="nav-link" >Listado</Link>                    
-                <Link to="/favoritos" className="nav-link" >Favoritos</Link>  
-                <Link to="/banderas" className="nav-link" >Banderas</Link>   
+                <Link to="/favoritos" className="nav-link" >Favoritos</Link>                 
                 <span className='d-flex text-white align-items-center'>       
              
                 
   </span>                      
             </div>
         </div>
-   
+  
      </div>
 </nav>
 
