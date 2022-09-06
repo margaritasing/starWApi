32a4591d326b001d5bda7b29283fb749680e728c
margaritasing <libiamfreitesm@gmail.com>
2022-09-06 17:38:57 -0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter} from "react-router-dom";
+
+
+import { StateProvider } from './reducer/StateProvider';
+import reducer, {inicialState} from './reducer/reducer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-       <App />    
-    </BrowserRouter>
+  <React.StrictMode>   
+    <StateProvider inicialState={inicialState} reducer ={reducer}> 
+       <App />  
+    </StateProvider>  
   </React.StrictMode>
 );
 
